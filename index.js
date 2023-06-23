6effb21b95c5aed8042d98d317ae97ea2851a7af
Syd Enciso <encisosydney19@gmail.com>
2023-06-23 11:36:19 +0800

    refactoring

--- index.js
+++ index.js
@@ -6,13 +6,142 @@ const io = new Server({
   },
 });
 
-io.on('connection', (socket) => {
-  socket.on('send_message', (data) => {
-    io.emit('receive_message', data);
-  });
+let list = [
+  {
+    id: 1,
+    phone: 'Nguyen',
+    name: 'Wright',
+    company: 'Buchanan',
+    agent: 'Goodwin',
+    lastOrderDate: 'Horton',
+    datePL: 'Abbott',
+    isCalling: false,
+  },
+  {
+    id: 2,
+    phone: 'Quinn',
+    name: 'Dennis',
+    company: 'Andrews',
+    agent: 'Schmidt',
+    lastOrderDate: 'Alexander',
+    datePL: 'Hardy',
+    isCalling: false,
+  },
+  {
+    id: 3,
+    phone: 'Carpenter',
+    name: 'Kelley',
+    company: 'Griffith',
+    agent: 'Daniels',
+    lastOrderDate: 'Barnett',
+    datePL: 'Lindsey',
+    isCalling: false,
+  },
+  {
+    id: 4,
+    phone: 'Carpenter',
+    name: 'Kelley',
+    company: 'Griffith',
+    agent: 'Daniels',
+    lastOrderDate: 'Barnett',
+    datePL: 'Lindsey',
+    isCalling: false,
+  },
+  {
+    id: 5,
+    phone: 'Carpenter',
+    name: 'Kelley',
+    company: 'Griffith',
+    agent: 'Daniels',
+    lastOrderDate: 'Barnett',
+    datePL: 'Lindsey',
+    isCalling: false,
+  },
+  {
+    id: 6,
+    phone: 'Carpenter',
+    name: 'Kelley',
+    company: 'Griffith',
+    agent: 'Daniels',
+    lastOrderDate: 'Barnett',
+    datePL: 'Lindsey',
+    isCalling: false,
+  },
+  {
+    id: 7,
+    phone: 'Carpenter',
+    name: 'Kelley',
+    company: 'Griffith',
+    agent: 'Daniels',
+    lastOrderDate: 'Barnett',
+    datePL: 'Lindsey',
+    isCalling: false,
+  },
+  {
+    id: 8,
+    phone: 'Carpenter',
+    name: 'Kelley',
+    company: 'Griffith',
+    agent: 'Daniels',
+    lastOrderDate: 'Barnett',
+    datePL: 'Lindsey',
+    isCalling: false,
+  },
+  {
+    id: 9,
+    phone: 'Carpenter',
+    name: 'Kelley',
+    company: 'Griffith',
+    agent: 'Daniels',
+    lastOrderDate: 'Barnett',
+    datePL: 'Lindsey',
+    isCalling: false,
+  },
+  {
+    id: 10,
+    phone: 'Carpenter',
+    name: 'Kelley',
+    company: 'Griffith',
+    agent: 'Daniels',
+    lastOrderDate: 'Barnett',
+    datePL: 'Lindsey',
+    isCalling: false,
+  },
+  {
+    id: 11,
+    phone: 'Carpenter',
+    name: 'Kelley',
+    company: 'Griffith',
+    agent: 'Daniels',
+    lastOrderDate: 'Barnett',
+    datePL: 'Lindsey',
+    isCalling: false,
+  },
+  {
+    id: 12,
+    phone: 'Carpenter',
+    name: 'Kelley',
+    company: 'Griffith',
+    agent: 'Daniels',
+    lastOrderDate: 'Barnett',
+    datePL: 'Lindsey',
+    isCalling: false,
+  },
+  {
+    id: 13,
+    phone: 'Carpenter',
+    name: 'Kelley',
+    company: 'Griffith',
+    agent: 'Daniels',
+    lastOrderDate: 'Barnett',
+    datePL: 'Lindsey',
+    isCalling: false,
+  },
+];
 
-  socket.on('toggle_calling', (data) => {
-    io.emit('list_calling', data);
+io.on('connection', (socket) => {
+  socket.on('filterContactList', (data) => {
+    io.emit('filteredContactList', list);
   });
 });
 
